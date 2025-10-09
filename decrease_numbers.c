6b21acdfb37fe69ec7a1dfdacbb51679d16b02cd
BryanKorir <Briankorir634@gmail.com>
2025-10-09 09:17:37 +0300

    Add files via upload

---
+++ decrease_numbers.c
@@ -0,0 +1,15 @@
+/*
+Name: Brian Kipkoech Korir
+Registration No: PA106/G/29216/25
+Description: program to display the for loop
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i;
+    for (i = 100; i >= 50; i--) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
